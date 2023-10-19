37c96249455b106425cf78f4d747b6aaebd25bdf
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-10-19 09:26:46 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace Villian_Names
+{
+    class Config
+    {
+        // assign information with no additional security to property
+        public static string congigString = @"DESKTOP-RE44NV4\SQLEXPRESS;Database=Minions;Integrated Security=true";
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Villian_Names
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {   
+            // create connection to SQL server and database
+            using SqlConnection sqlConnection = new SqlConnection(Config.congigString);
+          
+            // open connection 
+            sqlConnection.Open();
+
+            // implement generated SQL query using c#
+            string sqlQuery =  "SELECT v.Name, COUNT(mv.MinionId) AS MinionsCount" +
+                                 "FROM [Villains] AS v" +
+                            "LEFT JOIN MinionsVillains AS mv ON v.Id = mv.VillainId" +
+                             "GROUP BY v.Name" +
+                               "HAVING COUNT(mv.MinionId) > 3" +
+                             "ORDER BY MinionsCount";
+
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
+
+            int count = (int)sqlCommand.ExecuteScalar();
+            using SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            // close both connections  regardless of "using"
+            sqlConnection.Close();
+            reader.Close();
+        }
+    }
+}
